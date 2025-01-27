e3b7ab410c5506a48ca91d78036fc2dd59af89b8
Sofía <sofiavillarejo.dev@gmail.com>
2025-01-27 10:54:10 +0100

    Add project files.

---
+++ PrimerMvcNetCore/Controllers/PruebaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrimerMvcNetCore.Controllers
+{
+    public class PruebaController : Controller
+    {
+        //creamos metodo
+        public IActionResult PrimeraPagina()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
