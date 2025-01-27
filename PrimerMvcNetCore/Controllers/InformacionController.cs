92c7ad67679c7ee0d90bde0c25cf63da303d546d
Sofía <sofiavillarejo.dev@gmail.com>
2025-01-27 11:46:20 +0100

    primer mvc

---
+++ PrimerMvcNetCore/Controllers/InformacionController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimerMvcNetCore.Models;
+
+namespace PrimerMvcNetCore.Controllers
+{
+    public class InformacionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ControladorVista()
+        {
+            //vamos a enviar informacion simple a nuestra vista
+            ViewData["NOMBRE"] = "Alumno";
+            ViewData["EDAD"] = 23;
+            ViewBag.DiaSemana = "Lunes";
+            //enviar informacion compleja -> Model
+
+            return View();
+        }
+        public IActionResult ControladorVistaModel()
+        {
+            Persona persona = new Persona();
+            persona.Nombre = "Alumno";
+            persona.Email = "alumno@email.com";
+            persona.Edad = 23;
+            return View(persona);
+        }
+    }
+}
